36c7623e7862b1eacdbe87b6c4b922834a974af2
moberdor <moberdor@ucsc.edu>
2026-06-05 02:32:32 -0700

    add serve.py

---
+++ serve.py
@@ -0,0 +1,12 @@
+import http.server, socketserver
+
+class NoCacheHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
+        self.send_header('Pragma', 'no-cache')
+        self.send_header('Expires', '0')
+        super().end_headers()
+
+with socketserver.TCPServer(('', 8000), NoCacheHandler) as srv:
+    print('serving on http://localhost:8000')
+    srv.serve_forever()
